25dd249971ebe3b25d37bfe806bfbba088226e26
Justin Mak <justinmak2001@gmail.com>
2019-10-15 12:02:33 +0100

    Added 3-try feature

--- Week-3/numberGuess.py
+++ Week-3/numberGuess.py
@@ -1,14 +1,23 @@
 intro = 'Welcome to Number Guessing Game'
 print(intro)
 
-print('Pick a number between 1 and 10')
-guess = input()
-guess = int(guess)
-
 import random as r
 num = r.randint(1,10)
 
-if guess == num:
-    print('Nice you got it right')
-else:
-    print('You failed lol')
+print('Pick a number between 1 and 10. You have three tries.')
+
+i = 1 
+while i < 4:
+    print('This is guess number ' + str(i) + ':')
+    guess = input()
+    guess = int(guess)
+    if guess == num:
+        print('Nice you got it right')
+        break
+    elif guess > 10:
+        print("That's not even in the range fam")
+    else:
+        print('You failed lol')
+    i = i + 1
+
+print('Thanks for playing')
