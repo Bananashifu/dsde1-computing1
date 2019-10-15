575ffaab58d52ee5b9b403c0d8a040ff5b7c107d
Justin Mak <justinmak2001@gmail.com>
2019-10-15 12:07:54 +0100

    Cleaned by gorge

--- Week-3/numberGuess.py
+++ Week-3/numberGuess.py
@@ -1,5 +1,4 @@
-intro = 'Welcome to Number Guessing Game'
-print(intro)
+print('Welcome to Number Guessing Game')
 
 import random as r
 num = r.randint(1,10)
@@ -9,8 +8,7 @@ print('Pick a number between 1 and 10. You have three tries.')
 i = 1 
 while i < 4:
     print('This is guess number ' + str(i) + ':')
-    guess = input()
-    guess = int(guess)
+    guess = int(input())
     if guess == num:
         print('Nice you got it right')
         break
@@ -18,6 +16,6 @@ while i < 4:
         print("That's not even in the range fam")
     else:
         print('You failed lol')
-    i = i + 1
+    i += 1
 
 print('Thanks for playing')
