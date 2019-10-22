a0aae32a270d8abbe0dd141108bf68fba107072e
Justin Mak <justinmak2001@gmail.com>
2019-10-22 11:39:01 +0100

    pendulum.py exception handle update

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,7 +1,10 @@
 import math
 
 def period(L, g):
-    return 2 * math.pi * math.sqrt(L/g)
+    try:
+        return 2 * math.pi * math.sqrt(L/g)
+    except ZeroDivisionError or ValueError or TypeError:
+        print('Error: You are trying to divide by zero.')
 
 print('Input a value for L')
 L = int(input())
