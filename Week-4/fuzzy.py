f2f00abe4e5d072270f0e529156276e1fa8c52f0
Justin Mak <justinmak2001@gmail.com>
2019-11-08 16:29:07 +0000

    fuzzy and structures WIP

--- Week-4/fuzzy.py
+++ Week-4/fuzzy.py
@@ -6,22 +6,19 @@ Lint this file using PyLint.
 
 # This function does some maths on three numbers.
 def maths(input_a, input_b, input_c):
+    'Triples input_a, subtracts input_b, then adds input_c'
     result = input_a * 3 - input_b + input_c
     return result
 
 # This function returns True or False.
 def choices(question):
-    if(question == True):
+    'Returns True or False based on truthfulness of the question'
+    if question == True:
         return True
-    else:
-        return False
-
 
 def main():
-    this_variable = 'useless'
-    
     # first function takes three numbers
-    answer = maths(3, 9 , 2.3)
+    answer = maths(3, 9, 2.3)
     print(answer)
 
     # second function takes a True or False
