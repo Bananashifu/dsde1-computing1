f6894483a3a4cf3c5fd0182baac98ae89a62a8a0
Justin Mak <justinmak2001@gmail.com>
2019-11-10 14:43:53 +0000

    keywords

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -14,16 +14,37 @@ with default values.
 # it returns 'Hello and welcome to <place>'
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
-
+def welcome_message(user_name, place):
+    if user_name == '':
+        if place == '':
+            return 'Hello and welcome'
+        else:
+            return 'Hello and welcome to ' + place
+    else:
+        if place == '':
+            return 'Hello, ' + user_name + ', and welcome'
+        else:
+            return 'Hello, ' + user_name + ', and welcome to ' + place
 
 # Create a function called list_average()
 # without using any libraries to do the maths for you 
 # (the point of this exercise is to practice interacting 
 # with lists)
 # the first argument is a list of numbers
-# the second optional keyword arguemt is called avg_type
+# the second optional keyword argument is called avg_type
 # if nothing for avg_type provided, return the mean of the list
 # if avg_type='mode', return the mode of the list 
 # (return list of all modes if there is a tie between multiple values)
 # if avg_type='mean', return the mean of the list
 # if avg_type='median', return the median of this list
+def list_average(list, avg_type):
+    if avg_type == '' or 'mean':
+        return sum(list) / len(list)
+    elif avg_type == 'mode':
+        return max(set(list), key=list.count)
+    elif avg_type == 'median':
+        list.sort()
+        if len(list) % 2 == 1:
+            return list[len(list) // 2]
+        else:
+            return (list[int(len(list)/2)] + list[int(len(list)/2 - 1)]) / 2
